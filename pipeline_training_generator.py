9f6482dcbb4a08309c066b50eb6b422450dc86c4
Raymond Ko <wisely38@gmail.com>
2019-11-07 15:02:38 +0800

    feature - pipeline - fund training data generator module - update model

--- pipeline_training_generator.py
+++ pipeline_training_generator.py
@@ -23,14 +23,73 @@ OBJ = ["dobj", "pobj", "dobj"]
 
 
 
-def custom_seg(doc):
-    prev = doc[0].text
-    length = len(doc)
-    for index, token in enumerate(doc):
-        if (token.text == '.' and boundary.match(prev) and index!=(length - 1)):
-            doc[index+1].sent_start = False
-        prev = token.text
-    return doc
+# def custom_seg(doc):
+#     prev = doc[0].text
+#     length = len(doc)
+#     for index, token in enumerate(doc):
+#         if (token.text == '.' and boundary.match(prev) and index!=(length - 1)):
+#             doc[index+1].sent_start = False
+#         prev = token.text
+#     return doc
+
+# training data
+# TRAIN_DATA = [
+#     ("Who is Shaka Khan?", {"entities": [(7, 17, "PERSON")]}),
+#     ("I like London and Berlin.", {
+#      "entities": [(7, 13, "LOC"), (18, 24, "LOC")]}),
+# ]
+
+# train_data = [
+#     ("Uber blew through $1 million a week", [(0, 4, 'ORG')]),
+#     ("Android Pay expands to Canada", [(0, 11, 'PRODUCT'), (23, 30, 'GPE')]),
+#     ("Spotify steps up Asia expansion", [(0, 8, "ORG"), (17, 21, "LOC")]),
+#     ("Google Maps launches location sharing", [(0, 11, "PRODUCT")]),
+#     ("Google rebrands its business apps", [(0, 6, "ORG")]),
+#     ("look what i found on google! 😂", [(21, 27, "PRODUCT")])]
+
+# What entities do you need?
+# asset type - etf, indices,
+
+# product type - structured/off-shore fund
+# Risk level/type - credit risk/liquidity risk
+# company - HSBC
+
+
+# new entity label
+LABEL_ASSET_TYPE = "ASSET_TYPE"
+LABEL_RISK_TYPE = "RISK_TYPE"
+LABEL_EXPERTISE_TYPE = "EQUITY"
+LABEL_ASSETTYPE = "FIXED_INCOME"
+
+
+# training data
+# Note: If you're using an existing model, make sure to mix in examples of
+# other entity types that spaCy correctly recognized before. Otherwise, your
+# model might learn the new type, but "forget" what it previously knew.
+# https://explosion.ai/blog/pseudo-rehearsal-catastrophic-forgetting
+TRAIN_DATA = [
+    (
+        "Horses are too tall and they pretend to care about your feelings",
+        {"entities": [(0, 6, LABEL)]},
+    ),
+    ("Do they bite?", {"entities": []}),
+    (
+        "horses are too tall and they pretend to care about your feelings",
+        {"entities": [(0, 6, LABEL)]},
+    ),
+    ("horses pretend to care about your feelings", {"entities": [(0, 6, LABEL)]}),
+    (
+        "they pretend to care about your feelings, those horses",
+        {"entities": [(48, 54, LABEL)]},
+    ),
+    ("horses?", {"entities": [(0, 6, LABEL)]}),
+]
+
+
+
+def format_data(raw_data):
+    train_data_record = tuple()
+    train_data_record.    
 
 
 # needs to run first: python -m spacy download en_core_web_sm
@@ -42,13 +101,13 @@ def main():
     logger.info("Start processing sentence filter from folder:%s..."%subfolderpath)
     if os.path.exists(subfolderpath):
         for filename in os.listdir(subfolderpath):
-            if re.match("crawler-output_.+.txt", filename):
+            if re.match("filtered-output_.+.txt", filename):
                 logger.info("Start processing training data generator for file:%s..."%filename)
                 with open(os.path.normpath(os.path.join(os.getcwd(), subfolderpath, filename)), "r") as handler:
-                    filtered_filename = filename.replace("crawler-output_","filter-output_")
-                    nlp = spacy.load("en_core_web_sm")                    
+                    filtered_filename = filename.replace("filtered-output_","preparation-output_")
+                    nlp = spacy.load("en_core_web_lg")                    
                     # nlp.add_pipe(custom_seg, before='parser')
-                    handler.readlines()
+                    sentences = handler.readlines()
                     doc = nlp(text)
                     for sentence in doc.sents:
                         print(sentence.text)
@@ -78,12 +137,6 @@ def main():
 # 21 footprint [ pobj  ->  with / 92 / NOUN / 114 : 123 ]
 
 
-# training data
-# TRAIN_DATA = [
-#     ("Who is Shaka Khan?", {"entities": [(7, 17, "PERSON")]}),
-#     ("I like London and Berlin.", {
-#      "entities": [(7, 13, "LOC"), (18, 24, "LOC")]}),
-# ]
 
 
 
